d3e4727aa13d7b7b2ca937853bc7ae477d5d5d2b
Slobodan Djordjevic <slobodan.djordjevic@tamedia.ch>
2017-03-20 15:34:21 +0100

    Drawing now! Inner borders needs to be highlighted somehow.

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var dbSchema = mongoose.Schema({
 var Area = mongoose.model('Area', dbSchema);
 
 var data = '';
-var readStream = fs.createReadStream('CHE_adm3.kml', 'utf8');
+var readStream = fs.createReadStream('CHE_adm1.kml', 'utf8');
 
 readStream.on('data', function(chunk) {
     data += chunk;
@@ -28,33 +28,60 @@ readStream.on('data', function(chunk) {
   var coordsArray;
   var objToStore;
   var objToMongo;
+  var substringLev1;
+  var substringLev1_5;
+  var substringLev2;
+  var substringLev2_25
+  var substringLev2_5;
+  var substringLev3;
+  var substringLev4;
 
   for(var i=0;i<coordinates.length;i++){
     objToStore = {};
     objToStore.name = coordinates[i].match(/<name>(.*?)<\/name>/g).toString().replace(/<\/?name>/g,'');
     objToStore.description = coordinates[i].match(/<description>(.*?)<\/description>/g).toString().replace(/<\/?description>/g,'');
 
-    coordsArray = coordinates[i].match(/<coordinates>(.*?)<\/coordinates>/g).toString().split(" ");
-    coordsArray[0] = coordsArray[0].replace(/<\/?coordinates>/g,''); // Remove <coordinates> from first member of the array
-    coordsArray[coordsArray.length-1] = coordsArray[coordsArray.length-1].replace(/<\/?coordinates>/g,''); // Remove </coordinates> from last member of the array
+    substringLev1 = coordinates[i].match(/<MultiGeometry>(.*?)<\/MultiGeometry>/g).toString().replace(/<\/?MultiGeometry>/g,'');
+    substringLev1_5 = substringLev1.replace(/<\/Polygon>/g, '').replace(/<Polygon>/g,'')
+    substringLev2 = substringLev1_5.replace(/<outerBoundaryIs>/g, '').replace(/<LinearRing>/g, '').replace(/<coordinates>/g, '');
+    substringLev2_25 = substringLev2.replace(/<innerBoundaryIs>/g,'').replace(/<LinearRing>/g, '').replace(/<coordinates>/g, '');
+    substringLev2_5 = substringLev2_25.replace(/\t/g,'');
+    substringLev3 = substringLev2_5.replace(/<\/coordinates>/g,'').replace(/<\/LinearRing>/g, '').replace(/<\/outerBoundaryIs>/g, '|');
+    substringLev4 = substringLev3.replace(/<\/coordinates>/g,'').replace(/<\/LinearRing>/g, '').replace(/<\/innerBoundaryIs>/g, '|');
 
-    objToStore.coordinates = createPolyObject(coordsArray);
-    console.log(objToStore);
+    coordsArray = substringLev4.split("|");
+    coordsArray.splice(coordsArray.length-1, 1);
 
-    objToMongo = new Area(objToStore);
-    objToMongo.save(function(err, obj){
-      if (err) return console.error(err);
-    });
+    if(coordsArray.length == 1) {
+      objToStore.coordinates = createPolyObject(coordsArray[0].split(" "));
+
+      objToMongo = new Area(objToStore);
+      objToMongo.save(function(err, obj){
+        if (err) return console.error(err);
+      });
+    } else {
+      objToStore.coordinates = [];
+      for (var j=0;j<coordsArray.length;j++) {
+        objToStore.coordinates[j] = createPolyObject(coordsArray[j].split(" "));
+      }
+
+      objToMongo = new Area(objToStore);
+      objToMongo.save(function(err, obj){
+        if (err) return console.error(err);
+      });
+
+      console.log(i);
+    }
   }
 });
 
 function createPolyObject(arr) {
   var polyArray = [];
 
-  for (var i=0; i<arr.length;i++) {
+  for (var i=0; i<arr.length-1;i++) {
     polyArray.push({
-      lat: arr[i].split(',')[1],
-      lng: arr[i].split(',')[0]
+      lat: parseFloat(arr[i].split(',')[1]),
+      lng: parseFloat(arr[i].split(',')[0])
     })
   };
 
